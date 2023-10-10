b3f43c649438a43f37747d6c103fc0e5f3b4760f
edwardm8 <edwardm825@gmail.com>
2023-10-10 10:37:21 -0600

    setting up pages with vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: '/cpnt-260-a4',
+    base: '/cpnt-260-a4/',
     server:
     {
         host: true,
